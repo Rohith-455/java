601a831909cfd78d1fc5dddcc27a01c9389bdf40
Rohith Kumar A <rohithanbukumar02@gmail.com>
2025-01-30 23:05:37 +0800

    Add files via upload

---
+++ Java-Practice-Code-main/divide.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class divide{
+public static void main(String [] args){
+int n;
+Scanner sc = new Scanner(System.in);
+System.out.println("enter the number");
+n = sc.nextInt();
+if(n%27==0){
+System.out.println(n +"  the number is multiple of 27");
+}
+else{
+System.out.println(n +"  not an multiple");
+}
+}
+}  
