7b852694b322b8e36a6cb4f422b691b87281290c
Rohith Kumar A <rohithanbukumar02@gmail.com>
2025-01-30 19:37:17 +0800

    Add files via upload

---
+++ Java-Practice-Code-main/vowel,conso.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class switch{
+public static void main(String [] args){
+String r = "Z";
+switch(r)
+{
+case "a":
+System.out.println("vowel");
+break;
+case "e":
+System.out.println("vowel");
+break;
+case "i":
+System.out.println("vowel"):
+break;
+case"o":
+System.out.println("vowel"):
+break;
+case"u":
+System.out.println(" vowel");
+break;
+default:
+System.out.println("consonant");
+
+
